68c743f4ce6cd4f7fc3db04b1b1a898e0058ab59
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2022-02-09 10:56:22 -0800

    Add files via upload

---
+++ Non-linear/Node.java
@@ -0,0 +1,40 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.company;
+
+public class Node<T extends Number> {
+    private T data;
+    private Node<T> left;
+    private Node<T> right;
+
+    public Node(T item) {
+        data = item;
+    }
+
+    public T getData() {
+        return this.data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getLeft() {
+        return this.left;
+    }
+
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+
+    public Node<T> getRight() {
+        return this.right;
+    }
+
+    public void setRight(Node<T> right) {
+        this.right = right;
+    }
+}
